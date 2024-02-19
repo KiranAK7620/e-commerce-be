a18093e9eb681399eb274a7d12a3f0ca95b02086
Kiran Kadam <kadamkiran3179@gmail.com>
2024-02-19 17:57:50 +0530

    initial project setup

---
+++ src/main/java/com/kk/config/JwtConstant.java
@@ -0,0 +1,10 @@
+package com.kk.config;
+
+public class JwtConstant {
+
+	public static final String SECRET_KEY="bvsnwmenxwzodfizsxfffrultrerufcscigmtilluoixihxuaixyuchxdddf";
+	
+	public static final String JWT_HEADER="Authorization";
+	
+	
+}
